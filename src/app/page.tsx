73d8c1611fc1ac9b2ff9df16039a7e2ef945cfe1
Jo Seung Hyo <seunghyo7742@naver.com>
2023-10-17 16:25:41 +0900

    � Chore: commization pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-[375] h-[815] bg-cyan-100">
       home
-      <a href="http://localhost:3000">Next</a>
-      <a href="http://localhost:8000">Django</a>
-      <a href="http://localhost:8080">Spring</a>
+      <p className="text-[34] font-bold">Large Title</p>
+      <p className="text-[23] font-semibold">Medium Title</p>
+      <p className="text-[18] font-semibold">small Title</p>
+      <p className="text-[15] font-semibold">subtitle</p>
+      <p className="text-[16] font-bold">button bold</p>
+      <p className="text-[12] font-semibold">button semibold</p>
+      <p className="text-[13] font-medium">label medium</p>
+      <p className="text-[10] font-normal">label regular</p>
     </main>
   )
 }
