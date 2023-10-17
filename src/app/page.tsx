23f1cca76fa339ef9fb8a99bb53cc79ea1b19f69
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-10-17 18:01:21 +0900

    Revert "✨ Feat: entry 페이지 퍼블리싱"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,12 @@
-import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
-const languageArr = [
-  {
-    language: '한국어',
-  },
-  {
-    language: 'English',
-  },
-  {
-    language: 'にほんご',
-  },
-  {
-    language: 'Tiếng Việt',
-  },
-  {
-    language: '中國語',
-  },
-  {
-    language: 'नेपाली',
-  },
-]
-
-export default function Entry() {
+export default function Home() {
   return (
-    <main className="w-[23.4375rem] h-screen px-6 py-6 flex flex-col justify-around m-auto">
-      <h1>Please</h1>
-      <section>
-        <h2 className="font-semibold text-xl">Select Language</h2>
-        <div className="grid grid-cols-2 gap-4 mt-4">
-          {languageArr.map((data, index) => (
-            <Button
-              key={data.language}
-              className={`w-[9.4375rem] h-[5.6875rem] border-4 border-white rounded-lg text-xl text-black font-semibold ${
-                index % 2 ? 'bg-[#C2D471]' : 'bg-[#FAA339]'
-              } shadow-xl`}
-            >
-              {data.language}
-            </Button>
-          ))}
-        </div>
-      </section>
-      <Button className="w-full h-12 rounded-full font-bold text-base text-white bg-[#3CB371] shadow-md">Start</Button>
+    <main>
+      home
+      <a href="http://localhost:3000">Next</a>
+      <a href="http://localhost:8000">Django</a>
+      <a href="http://localhost:8080">Spring</a>
     </main>
   )
 }
