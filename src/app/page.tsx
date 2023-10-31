ee2b9dc2bce4d862251e782f6649ce1354dd19d9
taemin-Jang <jtm990506@gmail.com>
2023-10-31 14:58:48 +0900

    ✨ Feat: 각 언어 시각화를 위해 국기 표시
    - country_icons 추가
    - country component로 svg 이미지 동적으로 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
 import { Button } from '@/components/ui/button'
 import Logo from '#/please-logo.svg'
+import Country from '@/components/country'
+
 const languageArr = [
   {
     language: '한국어',
+    country: 'korea',
   },
   {
     language: 'English',
+    country: 'global',
   },
   {
     language: 'にほんご',
+    country: 'japan',
   },
   {
     language: 'Tiếng Việt',
+    country: 'vietnam',
   },
   {
     language: '中國語',
+    country: 'china',
   },
   {
     language: 'नेपाली',
+    country: 'nepal',
   },
 ]
 
@@ -26,15 +34,14 @@ export default function Entry() {
     <main className="w-[23.4375rem] h-screen px-6 py-6 flex flex-col justify-around m-auto">
       <Logo />
       <section>
-        <h2 className="font-semibold text-xl text-center">Select Language</h2>
-        <div className="grid grid-cols-2 gap-4 mt-4">
+        <h2 className="font-semibold text-2xl text-center">Select Language</h2>
+        <div className="grid grid-cols-2 gap-4 mt-10">
           {languageArr.map((data, index) => (
             <Button
               key={data.language}
-              className={`w-[9.4375rem] h-[5.6875rem] border-4 border-white rounded-lg text-xl text-black font-semibold ${
-                index % 2 ? 'bg-[#C2D471]' : 'bg-[#FAA339]'
-              } shadow-xl`}
+              className={`w-[9.4375rem] h-[5.6875rem] rounded-lg text-xl text-black font-semibold justify-evenly bg-[#f5f5f5] shadow-xl hover:bg-[#DDDDDD]`}
             >
+              <Country country={data.country} />
               {data.language}
             </Button>
           ))}
