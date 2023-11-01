2c9ef438ebbb356941f0c6e8eb83048cca994094
taemin-Jang <jtm990506@gmail.com>
2023-11-01 12:49:10 +0900

    ✨ Feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,30 @@
-import { Button } from '@/components/ui/button'
-import Logo from '#/please-logo.svg'
-import Country from '@/components/country'
+'use client'
+import PleaseLeft from '#/please-left.svg'
+import PleaseRight from '#/please-right.svg'
+import White from '#/white-handshake.svg'
+import Please from '#/please.svg'
 
-const languageArr = [
-  {
-    language: '한국어',
-    country: 'korea',
-  },
-  {
-    language: 'English',
-    country: 'global',
-  },
-  {
-    language: 'にほんご',
-    country: 'japan',
-  },
-  {
-    language: 'Tiếng Việt',
-    country: 'vietnam',
-  },
-  {
-    language: '中國語',
-    country: 'china',
-  },
-  {
-    language: 'नेपाली',
-    country: 'nepal',
-  },
-]
+import { useState } from 'react'
 
-export default function Entry() {
+export default function Landig() {
+  let [loading, setLoading] = useState(false)
+  setTimeout(() => setLoading(true), 2500)
+
   return (
-    <main className="w-[23.4375rem] h-screen px-6 py-6 flex flex-col justify-around m-auto">
-      <Logo />
-      <section>
-        <h2 className="font-semibold text-2xl text-center">Select Language</h2>
-        <div className="grid grid-cols-2 gap-4 mt-10">
-          {languageArr.map((data, index) => (
-            <Button
-              key={data.language}
-              className={`w-[9.4375rem] h-[5.6875rem] rounded-lg text-xl text-black font-semibold justify-evenly bg-[#f5f5f5] shadow-xl hover:bg-[#DDDDDD]`}
-            >
-              <Country country={data.country} />
-              {data.language}
-            </Button>
-          ))}
-        </div>
-      </section>
-      <Button className="w-full h-12 rounded-full font-bold text-base text-white bg-[#3CB371] shadow-md">Start</Button>
-    </main>
+    <div className="w-[23.4375rem] h-screen bg-[#44CD81] px-6 py-6  flex flex-col justify-around m-auto">
+      <div className=" relative group">
+        {!loading ? (
+          <div>
+            <PleaseLeft className="delay-500 animate-[handshake-right_2s_ease-in-out] fill-mode-both" />
+            <PleaseRight className="!absolute delay-500 right-0 top-0 animate-[handshake-left_2s_ease-in-out] fill-mode-both" />
+          </div>
+        ) : (
+          <div className="flex justify-center items-center gap-4">
+            <White />
+            <Please className="animate-[fade-in_0.5s_ease-in-out]" />
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
