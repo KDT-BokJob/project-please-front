ea475ad453ee68a5bcced577552c159611e5fd64
taemin-Jang <jtm990506@gmail.com>
2023-11-10 21:33:08 +0900

    🔧 Modify: jobCard Component 분리

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -4,56 +4,13 @@ import { FiSearch } from 'react-icons/fi'
 import { AiOutlineIdcard } from 'react-icons/ai'
 import { SlLocationPin } from 'react-icons/sl'
 import { IoIosArrowDown } from 'react-icons/io'
-import { PiFactoryLight } from 'react-icons/pi'
-import { FaBriefcase } from 'react-icons/fa'
-import { BiWon } from 'react-icons/bi'
-import { BsMap } from 'react-icons/bs'
-import { HiOutlineBookmark } from 'react-icons/hi'
-import { HiBookmark } from 'react-icons/hi'
 import { Button } from '@/components/ui/button'
 import Logo from '#/please-logo.svg'
 import SlickSlider from '@/components/slider'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import SelectModal from '@/app/jobs/select-modal'
 import { useRef, useState } from 'react'
-
-function JobCard({ recruit }: { recruit: any }) {
-  return (
-    <div className="relative flex flex-col gap-1 bg-base-bright-normal p-5 rounded-xl shadow-md">
-      {recruit.bookmark ? (
-        <HiBookmark className="absolute top-0 right-3" color="#3CB371" size="20" />
-      ) : (
-        <HiOutlineBookmark className="absolute top-0 right-3" color="#3CB371" size="18" />
-      )}
-      <span className="flex items-center justify-between">
-        <h2 className="font-semibold">{recruit.title}</h2>
-        <PiFactoryLight size="40" />
-      </span>
-      <span className="flex gap-2">
-        <FaBriefcase size="18" />
-        <p className="text-xs ">{recruit.work_type}</p>
-      </span>
-      <span className="flex gap-2">
-        <BiWon size="18" />
-        <p className="text-xs ">{recruit.salary.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} / month</p>
-      </span>
-      <span className="flex gap-2">
-        <BsMap size="18" />
-        <p className="text-xs ">{recruit.work_location}</p>
-      </span>
-      <span className="flex flex-wrap gap-1">
-        {recruit.visa_type.map((type: any) => (
-          <p
-            key={type}
-            className="inline-block px-8 py-1 border-2 text-xs bg-base-bright-light border-brand-secondary-light rounded-full"
-          >
-            {type}
-          </p>
-        ))}
-      </span>
-    </div>
-  )
-}
+import JobCard from '@/components/job-card'
 
 const jobData = [
   {
