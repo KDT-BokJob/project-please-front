6f169ac2f28c2e0496cc028423e47884e5b34007
taemin-Jang <jtm990506@gmail.com>
2023-11-10 17:37:55 +0900

    🔧 Modify: profile layout을 my path로 변경
    - profile page.tsx Button tag를 div로 변경

--- src/app/my/profile/page.tsx
+++ src/app/my/profile/page.tsx
@@ -1,21 +1,16 @@
 import { FaRegUserCircle } from 'react-icons/fa'
 import { PiUserCircleFill } from 'react-icons/pi'
 import { AiOutlineBell } from 'react-icons/ai'
-import { SlArrowLeft } from 'react-icons/sl'
 import { HiOutlineBookmark } from 'react-icons/hi'
 import { IoLanguage } from 'react-icons/io5'
 import { Button } from '@/components/ui/button'
+import Header from '@/components/ui/Header'
 
 export default function MyProfile() {
   return (
     <>
-      <div className="flex flex-col justify-center gap-8">
-        <div className="border-b border-base-secondary-normal">
-          <span className="px-2 mb-2 flex gap-2">
-            <SlArrowLeft size="20" />
-            <h1 className="headline">Profile Page</h1>
-          </span>
-        </div>
+      <Header headline="Profile Page" />
+      <div className="flex flex-col justify-center gap-8 mt-14">
         <span className="flex gap-6 ml-6 b">
           <PiUserCircleFill size="60" color="#3CB371" />
           <div className="flex flex-col justify-center">
@@ -24,30 +19,30 @@ export default function MyProfile() {
           </div>
         </span>
         <div className="border-t border-base-secondary-normal">
-          <Button className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
+          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
             <span className="flex ml-10 py-3 items-center gap-9">
               <FaRegUserCircle size="30" />
               <p>Resume</p>
             </span>
-          </Button>
-          <Button className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
+          </div>
+          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
             <span className="flex ml-10 py-3 items-center gap-9">
               <AiOutlineBell size="30" />
               <p>Push Notifications</p>
             </span>
-          </Button>
-          <Button className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
+          </div>
+          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
             <span className="flex ml-10 py-3 items-center gap-9">
               <HiOutlineBookmark size="30" />
               <p>Bookmarks</p>
             </span>
-          </Button>
-          <Button className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
+          </div>
+          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
             <span className="flex ml-10 py-3 items-center gap-9">
               <IoLanguage size="30" />
               <p>Language</p>
             </span>
-          </Button>
+          </div>
         </div>
       </div>
       <div className="flex flex-col justify-end px-7 mb-10 gap-4">
