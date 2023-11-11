d184bf3ea4f4aa880f4f10ac6b1964ff3c450b70
taemin-Jang <jtm990506@gmail.com>
2023-11-11 13:17:28 +0900

    ✨ Feat: applications(지원 현황) page 퍼블리싱
    - JobCard 컴포넌트에 children props를 추가하여 Review 버튼 추가함

--- src/components/job-card.tsx
+++ src/components/job-card.tsx
@@ -4,7 +4,7 @@ import { FaBriefcase } from 'react-icons/fa'
 import { HiBookmark, HiOutlineBookmark } from 'react-icons/hi'
 import { PiFactoryLight } from 'react-icons/pi'
 
-export default function JobCard({ recruit }: { recruit: any }) {
+export default function JobCard({ recruit, children }: { recruit: any; children?: React.ReactNode }) {
   return (
     <div className="relative flex flex-col gap-1 bg-base-bright-normal p-5 rounded-xl shadow-md">
       {recruit.bookmark ? (
@@ -28,16 +28,17 @@ export default function JobCard({ recruit }: { recruit: any }) {
         <BsMap size="18" />
         <p className="text-xs ">{recruit.work_location}</p>
       </span>
-      <span className="flex flex-wrap gap-1">
+      <span className="w-3/4 grid grid-cols-3 gap-1 mt-2">
         {recruit.visa_type.map((type: any) => (
           <p
             key={type}
-            className="inline-block px-8 py-1 border-2 text-xs bg-base-bright-light border-brand-secondary-light rounded-full"
+            className="inline-block py-1 border-2 text-xs text-center bg-base-bright-light border-brand-secondary-light rounded-full"
           >
             {type}
           </p>
         ))}
       </span>
+      {children}
     </div>
   )
 }
