6d25e0a5d2dbc5c27bbf4c03f787366f390ae4fc
taemin-Jang <jtm990506@gmail.com>
2023-11-13 01:20:25 +0900

    ✨ Feat: 구직자 profile에서 언어 선택 모달창 추가
    - shadcn dialog로 구현
    - css 수정

--- src/app/my/profile/page.tsx
+++ src/app/my/profile/page.tsx
@@ -5,6 +5,36 @@ import { HiOutlineBookmark } from 'react-icons/hi'
 import { IoLanguage } from 'react-icons/io5'
 import { Button } from '@/components/ui/button'
 import Header from '@/components/ui/Header'
+import Link from 'next/link'
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from '@/components/ui/dialog'
+import Country from '@/components/country'
+
+const languageArr = [
+  {
+    language: '한국어',
+    country: 'korea',
+  },
+  {
+    language: 'English',
+    country: 'global',
+  },
+  {
+    language: 'にほんご',
+    country: 'japan',
+  },
+  {
+    language: 'Tiếng Việt',
+    country: 'vietnam',
+  },
+  {
+    language: '中國語',
+    country: 'china',
+  },
+  {
+    language: 'नेपाली',
+    country: 'nepal',
+  },
+]
 
 export default function MyProfile() {
   return (
@@ -19,30 +49,74 @@ export default function MyProfile() {
           </div>
         </span>
         <div className="border-t border-base-secondary-normal">
-          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
+          {/* 이력서 */}
+          <Link
+            href="/resume/profile"
+            className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal cursor-pointer hover:text-brand-primary-normal"
+          >
             <span className="flex ml-10 py-3 items-center gap-9">
               <FaRegUserCircle size="30" />
               <p>Resume</p>
             </span>
-          </div>
-          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
+          </Link>
+          {/* 푸쉬 알람 */}
+          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal cursor-pointer hover:text-brand-primary-normal">
             <span className="flex ml-10 py-3 items-center gap-9">
               <AiOutlineBell size="30" />
               <p>Push Notifications</p>
             </span>
           </div>
-          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
+          {/* 북마크 */}
+          <Link
+            href="bookmarks"
+            className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal cursor-pointer hover:text-brand-primary-normal"
+          >
             <span className="flex ml-10 py-3 items-center gap-9">
               <HiOutlineBookmark size="30" />
               <p>Bookmarks</p>
             </span>
-          </div>
-          <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal">
-            <span className="flex ml-10 py-3 items-center gap-9">
-              <IoLanguage size="30" />
-              <p>Language</p>
-            </span>
-          </div>
+          </Link>
+          {/* 언어 선택 */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <div className="block w-full rounded-none p-0 h-auto border-b border-base-secondary-normal cursor-pointer hover:text-brand-primary-normal">
+                <span className="flex ml-10 py-3 items-center gap-9">
+                  <IoLanguage size="30" />
+                  <p>Language</p>
+                </span>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="items-center">
+                <span className="w-14 h-14 flex justify-center items-center rounded-lg bg-[#F5F5F5]">
+                  <IoLanguage size="30" />
+                </span>
+              </DialogHeader>
+              <div className="grid grid-cols-2 gap-4">
+                {languageArr.map((data) => (
+                  <p
+                    key={data.language}
+                    className={`text-xl text-black font-semibold flex justify-center gap-2 hover:text-brand-primary-normal`}
+                  >
+                    <Country country={data.country} />
+                    {data.language}
+                  </p>
+                ))}
+              </div>
+              <DialogFooter className="flex flex-row justify-center sm:justify-center gap-8">
+                <DialogClose asChild>
+                  <Button className="w-24 h-12 rounded-lg font-bold text-base border border-brand-primary-normal text-brand-primary-normal bg-base-bright-light shadow-md">
+                    취소
+                  </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Button className="w-24 h-12 rounded-lg font-bold text-base text-base-bright-light bg-brand-primary-normal shadow-md">
+                    확인
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
       <div className="flex flex-col justify-end px-7 mb-10 gap-4">
@@ -50,11 +124,11 @@ export default function MyProfile() {
           Log out
         </Button>
         <span className="flex justify-around paragraph text-base-secondary-light">
-          <p>이용약관</p>
+          <p className="cursor-pointer">이용약관</p>
           <p>|</p>
-          <p>개인정보처리방침</p>
+          <p className="cursor-pointer">개인정보처리방침</p>
           <p>|</p>
-          <p>회원탈퇴</p>
+          <p className="cursor-pointer">회원탈퇴</p>
         </span>
       </div>
     </>
