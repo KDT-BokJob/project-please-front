65b5f3b5e60999f2a83299cb06b036facefc0e7f
taemin-Jang <jtm990506@gmail.com>
2023-11-13 09:40:08 +0900

    ✨ Feat: job detail page에서 apply 버튼 클릭 시 modal 창 띄움
    - 나중에 이력서 작성 여부에 따라 모달창이 띄워줘야함함

--- src/app/jobs/detail/[id]/page.tsx
+++ src/app/jobs/detail/[id]/page.tsx
@@ -4,6 +4,8 @@ import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { Button } from '@/components/ui/button'
 import { HiOutlineBookmark } from 'react-icons/hi'
 import { HiBookmark } from 'react-icons/hi'
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from '@/components/ui/dialog'
+import { BsCheckCircle } from 'react-icons/bs'
 
 const jobData = {
   recruit_id: 1,
@@ -87,9 +89,35 @@ export default function JobsDetail() {
               Mark
             </Button>
           )}
-          <Button className="w-full h-12 rounded-full font-bold text-base text-base-bright-light bg-brand-primary-normal shadow-md">
-            Apply
-          </Button>
+          {/* Apply 버튼 클릭 시 모달 창 띄우기 (나중에 이력서 여부에 따라 변경하게 해야함) */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="w-full h-12 rounded-full font-bold text-base text-base-bright-light bg-brand-primary-normal shadow-md">
+                Apply
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="items-center">
+                <span className="w-14 h-14 flex justify-center items-center rounded-lg bg-[#F5F5F5]">
+                  <BsCheckCircle size="30" />
+                </span>
+              </DialogHeader>
+              <p className="text-center text-xl font-semibold">Sure you want to appy without Resume?</p>
+              <p className="text-center text-xs">Writing resume makes your chances of passing higher</p>
+              <DialogFooter className="flex flex-row justify-center sm:justify-center gap-8">
+                <DialogClose asChild>
+                  <Button className=" h-12 rounded-lg font-bold text-base border border-brand-primary-normal text-brand-primary-normal bg-base-bright-light shadow-md">
+                    Go Edit
+                  </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Button className=" h-12 rounded-lg font-bold text-base text-base-bright-light bg-brand-primary-normal shadow-md">
+                    Yes, Confirm
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </span>
       </div>
     </>
