54782ec189753a8e7e60db37754d8e1fefa01e01
taemin-Jang <jtm990506@gmail.com>
2023-11-15 01:51:01 +0900

    💄 Design: 아이콘 추가 및 css 수정

--- src/app/jobs/detail/[id]/page.tsx
+++ src/app/jobs/detail/[id]/page.tsx
@@ -2,37 +2,58 @@ import Image from 'next/image'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from '@/components/ui/dialog'
-import { ChevronLeft, CheckedBookmarkIcon, OutlineBookmarkIcon, CheckCircleIcon } from '@/lib/icons'
+import {
+  ChevronLeft,
+  PercentIcon,
+  KRWIcon,
+  BriefcaseIcon,
+  VisaIcon,
+  ReloadIcon,
+  CheckCircleIcon,
+  CheckedBookmarkIcon,
+  OutlineBookmarkIcon,
+} from '@/lib/icons'
 
 const jobData = {
   recruit_id: 1,
   company_id: 123,
   job_code: 'a123',
-  title: '공고명1',
+  title: '코딩 가르쳐주실 분 급구',
   content: '세부 내용',
   create_at: '작성 날짜',
-  expired_at: '마감일',
+  expired_at: '1월13일',
   salary: '3000000',
   work_type: '제조업',
-  work_location: 'Gwangjin-gu/dddd/dddd',
+  work_location: '서울특별시/마포구',
+  work_start_data: '근무시작일',
+  work_end_data: '근무종료일',
   visa_type: ['E-7', 'E-9'],
-  work_period: '근무기간',
-  work_days_week: '주총근무일수',
+  work_period: '6개월',
+  work_days_week: '주 5일',
+  is_work_week_agreement: true,
   work_start_hour: '근무시작시간',
   work_end_hour: '근무종료시간',
+  is_work_time_agreement: true,
+  gender: '여성',
+  salary_type: '월급',
+  contry: '베트남',
+  recruited_number: 3,
+  is_visa_transform: true,
   bookmark: true,
+  employee_count: 10,
+  foreign_employee_count: 3,
 }
 
-function Detail({ type, value }: { type: string; value: any }) {
+function Detail({ type, value, className }: { type: string; value: any; className?: string }) {
   return (
-    <span className="flex justify-between">
-      <p className="font-semibold text-s">{type}</p>
+    <span className="flex w-full justify-between">
+      <p className={`font-semibold text-sm ${className}`}>{type}</p>
       <p className="text-sm text-base-secondary-dark">{value}</p>
     </span>
   )
 }
 
-export default function JobsDetail() {
+export default function EmpoyerJobPostingPreviewPage() {
   return (
     <>
       <div className="px-2 mb-2">
@@ -50,25 +71,77 @@ export default function JobsDetail() {
       </div>
       <div className="px-6 flex flex-col gap-8 mb-8">
         <div>
-          <h1 className="font-bold text-lg">{jobData.title}</h1>
-          <Detail type="기업명" value={jobData.create_at} />
+          <h1 className="font-bold text-xl">{jobData.title}</h1>
+          <Detail className="text-base-secondary-light" type="복잡한 코딩" value={`~${jobData.expired_at}`} />
         </div>
         <div className="flex flex-col gap-2">
-          <Detail type="Job Type" value={jobData.work_type} />
+          <span className="flex gap-2">
+            <BriefcaseIcon />
+            <Detail className="text-brand-primary-normal" type="직무" value={jobData.work_type} />
+          </span>
           <hr />
-          <Detail type="Location" value={jobData.work_location} />
+          <span className="flex gap-2">
+            <VisaIcon />
+            <Detail className="text-brand-primary-normal" type="비자" value={jobData.visa_type.join(' ')} />
+          </span>
           <hr />
-          <Detail type="Visa" value={jobData.visa_type.join(' ')} />
+          <span className="flex gap-2">
+            <ReloadIcon />
+            <Detail
+              className="text-brand-primary-normal"
+              type="비자 전환"
+              value={jobData.is_visa_transform ? '가능' : '불가능'}
+            />
+          </span>
+          <hr />
+          <span className="flex gap-2">
+            <PercentIcon />
+            <Detail
+              className="text-brand-primary-normal"
+              type="외국인 비율"
+              value={`${(jobData.foreign_employee_count / jobData.employee_count) * 100}%`}
+            />
+          </span>
+          <hr />
+          <span className="flex gap-2">
+            <KRWIcon />
+            <Detail
+              className="text-brand-primary-normal"
+              type="급여"
+              value={`${jobData.salary.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}/${jobData.salary_type}`}
+            />
+          </span>
         </div>
         <div className="flex flex-col gap-2">
-          <h2 className="font-bold text-lg">Working Conditions</h2>
-          <Detail type="Working Period" value={jobData.work_period} />
+          <h2 className="font-semibold">근무 조건</h2>
+          <Detail className="text-brand-primary-normal" type="기간" value={jobData.work_period} />
           <hr />
-          <Detail type="Working days" value={jobData.work_days_week} />
+          <Detail className="text-brand-primary-normal" type="요일" value={jobData.work_days_week} />
+          {jobData.is_work_week_agreement ? (
+            <p className="text-xs text-base-secondary-light text-right">*협의 가능</p>
+          ) : (
+            <></>
+          )}
           <hr />
-          <Detail type="Working hours" value={`${jobData.work_start_hour} - ${jobData.work_end_hour}`} />
+          <Detail
+            className="text-brand-primary-normal"
+            type="시간"
+            value={`${jobData.work_start_hour} - ${jobData.work_end_hour}`}
+          />
+          {jobData.is_work_time_agreement ? (
+            <p className="text-xs text-base-secondary-light text-right">*협의 가능</p>
+          ) : (
+            <></>
+          )}
           <hr />
-          <Detail type="Pay" value={`${jobData.salary.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}/month`} />
+          <Detail className="text-brand-primary-normal" type="희망 국적" value={jobData.contry} />
+          <hr />
+          <Detail className="text-brand-primary-normal" type="모집 성별" value={jobData.gender} />
+          <hr />
+          <Detail className="text-brand-primary-normal" type="모집 인원" value={`${jobData.recruited_number}명`} />
+          <hr />
+          <Detail className="text-brand-primary-normal" type="위치" value={jobData.work_location} />
+          <hr />
         </div>
         <div className="flex flex-col">
           <h2 className="font-bold text-lg text-base-primary-normal">Job Description</h2>
