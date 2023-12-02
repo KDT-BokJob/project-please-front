b318b924092669e285bbe64f5f18da53bac5584e
taemin-Jang <jtm990506@gmail.com>
2023-12-03 08:32:54 +0900

    ✨ Feat: 번역 데이터 추가
    - desired job

--- src/app/(employee)/[locale]/resume/desired-job/[visa]/page.tsx
+++ src/app/(employee)/[locale]/resume/desired-job/[visa]/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { zodResolver } from '@hookform/resolvers/zod'
-import React from 'react'
+import { TFunction } from 'i18next'
+import React, { useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
+import initTranslations from '@/app/i18n'
 import { Button } from '@/components/ui/button'
 import { CheckboxButton } from '@/components/ui/checkboxButton'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
@@ -56,10 +58,23 @@ const visaType = [
   },
 ]
 const formSchema = resumeDesiredJobFormSchema
-export default function page({ params }: { params: { visa: string } }) {
+export default function page({ params: { locale, visa } }: { params: { locale: string; visa: string } }) {
+  let tl = useRef<TFunction<['translation', ...string[]], undefined>>()
+  const [currentLang, setCurrentLang] = useState('')
+
+  useEffect(() => {
+    const translate = async () => {
+      const { t, language } = await initTranslations(locale, ['desired', 'common'])
+      tl.current = t
+      setCurrentLang(language)
+      console.log(visa)
+    }
+    translate()
+  }, [])
+
   let targetJobs: any[] = []
   visaType.forEach((obj) => {
-    if (obj.visa === params.visa) targetJobs = obj.jobs
+    if (obj.visa === visa) targetJobs = obj.jobs
   })
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -72,9 +87,12 @@ export default function page({ params }: { params: { visa: string } }) {
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
   }
+
+  if (!tl.current) return null
+
   return (
     <>
-      <h2 className={'title-m mx-auto mt-[135px] mb-[70px]'}>✨Desired job</h2>
+      <h2 className={'title-m mx-auto mt-[135px] mb-[70px]'}>✨{tl.current('Desired_job')}</h2>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
@@ -104,10 +122,10 @@ export default function page({ params }: { params: { visa: string } }) {
           />
           <div className="flex justify-between gap-4 mt-auto">
             <Button variant={'innerLine'} size={'lg'}>
-              Back
+              {tl.current('common:Back')}
             </Button>
             <Button type="submit" variant={'primary'} size={'lg'}>
-              Next
+              {tl.current('common:Next')}
             </Button>
           </div>
         </form>
